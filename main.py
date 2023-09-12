acf84685a637e5c417df29197831279feb058c82
vit4o021 <oliveiravitao68@gmail.com>
2023-09-12 17:22:19 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+from skimage import io
+import matplotlib.pyplot as plt
+
+
+image = io.imread('https://github.com/vit4o021/TRAB/blob/main/homemaranha.jpg?raw=true')
+plt.imshow(image)
+plt.show()
+
